72da93f0b1365cbeeea17771d3944e84eadd2f70
oussamabonnor1 <oussamabonnor@gmail.com>
2020-05-10 23:06:51 +0100

    fix

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ import json
 import os
 
 database_name = "capstone"
-database_path = os.environ["DATABASE_URL", "postgres://{}:{}@{}/{}".format('postgres', '','localhost:5432', database_name)] 
+database_path = os.environ.get("DATABASE_URL", "postgres://{}:{}@{}/{}".format('postgres', '','localhost:5432', database_name))
 
 db = SQLAlchemy()
 
